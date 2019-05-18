837a867eab2372ce9009e8d35d213e2b5e2aeeb8
Matias Camiletti <matias.camiletti@gmail.com>
2019-05-18 13:09:04 -0300

    Quitado campo de response.

--- src/Diactoros/MiaJsonErrorResponse.php
+++ src/Diactoros/MiaJsonErrorResponse.php
@@ -5,7 +5,6 @@ class MiaJsonErrorResponse extends \Zend\Diactoros\Response\JsonResponse
     public function __construct($code, $message) {
         parent::__construct(array(
             'success' => false,
-            'response' => false,
             'error' => array(
                 'code' => $code,
                 'message' => $message
