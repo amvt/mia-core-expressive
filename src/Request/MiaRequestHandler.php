378bd3ae3fa7624209127a6636b5ccf0c0acb5ef
Matias Camiletti <matias.camiletti@gmail.com>
2019-10-29 11:16:56 -0300

    Agregado funcionalidad para verificar el tipo de respuesta.

--- src/Request/MiaRequestHandler.php
+++ src/Request/MiaRequestHandler.php
@@ -3,6 +3,21 @@
 abstract class MiaRequestHandler implements \Psr\Http\Server\RequestHandlerInterface
 {
     /**
+     * Verifica el tipo de respuesta que el cliente quiere ejecutar
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param mixed $data
+     * @return \Mobileia\Expressive\Diactoros\MiaCsvResponse|\Mobileia\Expressive\Diactoros\MiaJsonResponse
+     */
+    public function response(\Psr\Http\Message\ServerRequestInterface $request, $data)
+    {
+        $export = $this->getParam($request, 'export', '');
+        if($export == 'csv'){
+            return new \Mobileia\Expressive\Diactoros\MiaCsvResponse($data);
+        }
+        
+        return new \Mobileia\Expressive\Diactoros\MiaJsonResponse($data);
+    }
+    /**
      * Obtener parametro sin importar de donde provenga.
      */
     public function getParam(\Psr\Http\Message\ServerRequestInterface $request, $key, $default = null)
